4c182bede5d2840d60b4bdee6f8512057e0af120
sofia <88784252+5of1a@users.noreply.github.com>
2024-05-29 19:03:39 +0200

    update common.js 29.05.2024

--- js/common.js
+++ js/common.js
@@ -1,41 +1,56 @@
 /* JS Code, der nur auf mehreren Pages verwendet wird */
 
-document.addEventListener("DOMContentLoaded", function () {
-  const form = document.querySelector("form");
+const form = document.querySelector("form");
+const spinner = document.getElementById("spinner");
 
-  form.addEventListener("submit", function (event) {
-    event.preventDefault();
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
 
-    const name = document.getElementById("name");
-    const email = document.getElementById("email");
-    const tel = document.getElementById("tel");
-    const nameValue = name.value.trim();
-    const telValue = tel.value.trim();
-    const emailValue = email.value.trim();
+  const name = document.getElementById("name");
+  const email = document.getElementById("email");
+  const tel = document.getElementById("tel");
+  const text = document.getElementById("text");
+  const nameValue = name.value.trim();
+  const telValue = tel.value.trim();
+  const emailValue = email.value.trim();
 
-    function displayError(inputField, message) {
-      const errorDisplay = inputField.nextElementSibling;
-      errorDisplay.textContent = message;
-      errorDisplay.style.display = "block";
-    }
+  function displayError(inputField, message) {
+    const errorDisplay = inputField.nextElementSibling;
+    errorDisplay.textContent = message;
+    errorDisplay.style.display = "block";
+  }
 
-    document
-      .querySelectorAll(".error")
-      .forEach((e) => (e.style.display = "none"));
+  document
+    .querySelectorAll(".error")
+    .forEach((e) => (e.style.display = "none"));
 
-    if (nameValue === "") {
-      displayError(name, "Bitte geben Sie Ihren vollständigen Namen ein.");
-      return;
-    }
-    if (!emailValue.includes("@")) {
-      displayError(email, "Bitte geben Sie eine gültige E-Mail-Adresse ein.");
-      return;
-    }
-    if (telValue === "") {
-      displayError(tel, "Bitte geben Sie Ihre Telefonnummer ein.");
-      return;
-    }
+  if (nameValue === "") {
+    displayError(name, "Bitte geben Sie Ihren vollständigen Namen ein.");
+    return;
+  }
+  if (!emailValue.includes("@")) {
+    displayError(email, "Bitte geben Sie eine gültige E-Mail-Adresse ein.");
+    return;
+  }
+  if (telValue === "") {
+    displayError(tel, "Bitte geben Sie Ihre Telefonnummer ein.");
+    return;
+  }
 
-    form.submit();
-  });
+  spinner.style.display = "block";
+
+  sendToDatabase(nameValue, emailValue, telValue, text);
 });
+
+const sendToDatabase = async (nameValue, emailValue, telValue, text) => {
+  await databaseClient.insertInto("user", {
+    email: emailValue,
+    name: nameValue,
+    tel: telValue,
+    text: text.value,
+  });
+
+  spinner.style.display = "none";
+
+  form.submit();
+};
